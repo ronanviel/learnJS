eec989c188478188bddab7746213aa276836bbcf
Ronan Viel <ronan.viel@orange.com>
2026-05-28 10:16:12 -0400

    scope practice

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -12,12 +12,12 @@ const blockConst = "I'm a block-scoped const";
 }
 
 // Global scope
-console.log(globalVar); // Output: "I'm a global variable"
-console.log(globalLet); // Output: "I'm also global, but scoped with let"
-console.log(globalConst); // Output: "I'm a global constant"
+//console.log(globalVar); // Output: "I'm a global variable"
+//console.log(globalLet); // Output: "I'm also global, but scoped with let"
+//console.log(globalConst); // Output: "I'm a global constant"
 
 //Block Scope
-console.log(blockVar);
+//console.log(blockVar);
 //console.log(blockLet);
 
 function show(){
@@ -27,6 +27,19 @@ const functionConst = "I'm a block-scoped const";
 }
 show();
 
-console.log(functionVar); // Throws ReferenceError
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceError
+//console.log(functionVar); // Throws ReferenceError
+//console.log(functionLet); // Throws ReferenceError
+//console.log(functionConst); // Throws ReferenceError
+
+{
+    let var1 = 1
+    const CONSTANT = 1
+    var var2 = 2
+
+    var1 = 2
+ //   CONSTANT = 2
+    var2 = 3
+}
+var1 = 4
+var2 = 4
+console.log(var1)
