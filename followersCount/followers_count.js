06389bd8a44057d4b241b6b511f92c71d9733119
Ronan Viel <ronan.viel@orange.com>
2026-05-29 05:33:49 -0400

    add checkCountValue

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -3,6 +3,15 @@ let count = 0;
 function increaseCount() {
     count++;
     displayCount();
+    checkCountValue();
+}
+
+function checkCountValue() {
+    if (count === 10) {
+        alert("Your Instagram post gained 10 followers! Congratulations!");
+    } else if (count === 20) {
+        alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
 }
 
 function displayCount() {
